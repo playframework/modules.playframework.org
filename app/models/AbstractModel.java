59235cad75ebbf34b6d6272a493752c35d94a3fc
Steve Chaloner <github-NOSPAM@objectify.be>
2012-04-16 10:42:00 +0200

    added MappedSuperclass to AbstractModel

--- app/models/AbstractModel.java
+++ app/models/AbstractModel.java
@@ -1,7 +1,9 @@
 package models;
 
 import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 
+@MappedSuperclass
 public abstract class AbstractModel extends play.db.ebean.Model {
 
     @Id
