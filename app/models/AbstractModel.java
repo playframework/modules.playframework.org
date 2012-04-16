b3440ec6e278bfab18c167d0c4e9b98d33919b73
Steve Chaloner <github-NOSPAM@objectify.be>
2012-04-16 13:29:30 +0200

    Updated Build.scala to point to objectify module repo

--- app/models/AbstractModel.java
+++ app/models/AbstractModel.java
@@ -4,13 +4,13 @@ import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
 @MappedSuperclass
-public abstract class AbstractModel extends play.db.ebean.Model {
-
+public abstract class AbstractModel extends play.db.ebean.Model
+{
     @Id
     public Long id;
 
-    public Long getId() {
+    public Long getId()
+    {
         return id;
     }
-
 }
--- project/Build.scala
+++ project/Build.scala
@@ -8,11 +8,11 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      "be.objectify" % "deadbolt-2_2.9.1" % "1.1.2"
+      "deadbolt-2" %% "deadbolt-2" % "1.1.2"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
-        resolvers += "Local Play Repository" at "file:///home/steve/development/play/play-2.0/repository/local"
+        resolvers += "Objectify Play Repository" at "http://schaloner.github.com/releases/"
     )
 
 }
